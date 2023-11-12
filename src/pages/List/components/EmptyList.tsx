abe1409b50e8526bf1760f5a73a5b2de3939b054
Ali Valipour <ali.valipour@email.kntu.ac.ir>
2023-11-13 01:53:36 +0330

    fix: List addNote Butoon navigating

--- src/pages/List/components/EmptyList.tsx
+++ src/pages/List/components/EmptyList.tsx
@@ -1,13 +1,21 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { Button } from "../../../components";
 
 export const EmptyList: FunctionComponent = () => {
+  const navigate = useNavigate();
+
+  const addNote = useCallback(() => navigate("new"), [navigate]);
+
   return (
     <div className="flex h-full flex-col items-center justify-center gap-3 rounded-xl bg-gray-50">
       <div className=" text-2xl font-normal text-[#99999B]">
         No note added to this device yet
       </div>
-      <Button leftIcon>Add Note</Button>
+      <Button leftIcon onClick={addNote}>
+        Add Note
+      </Button>
     </div>
   );
 };
