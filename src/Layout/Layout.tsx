d33cc0105281cd1071da98edd46c40e243c871b7
Ali Valipour <ali.valipour@email.kntu.ac.ir>
2023-11-13 01:52:52 +0330

    fix: Layout nested routes

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,13 +1,16 @@
-import { FunctionComponent } from "react";
-import { Outlet } from "react-router-dom";
+import { FunctionComponent, lazy } from "react";
 
 import { Header } from "../modules";
+import { Outlet } from "react-router-dom";
+
+const List = lazy(() => import("../pages/List"));
 
 export const Layout: FunctionComponent = () => {
   return (
     <div className="flex h-[100dvh] flex-col font-['Lato']">
       <Header />
       <div className="mx-6 mb-8 mt-4 h-full">
+        <List />
         <Outlet />
       </div>
     </div>
