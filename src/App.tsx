bd7ebd8fa3d209127a89ef95fe275574887c63b3
Ali Valipour <ali.valipour@email.kntu.ac.ir>
2023-11-13 21:26:16 +0330

    fix: remove lazy loading for fixing github pages deploymant

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Loader } from "./components/Loader";
-
-const Layout = lazy(() => import("./Layout"));
-const New = lazy(() => import("./pages/New"));
+import Layout from "./Layout";
+import New from "./pages/New";
 
 const App = () => {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Layout />
-            </Suspense>
-          }
-        >
-          <Route
-            path="new"
-            element={
-              <Suspense fallback={<Loader />}>
-                <New />
-              </Suspense>
-            }
-          />
+        <Route path="/" element={<Layout />}>
+          <Route path="new" element={<New />} />
         </Route>
       </Routes>
     </BrowserRouter>
