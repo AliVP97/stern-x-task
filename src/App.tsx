8cb85138ebf245789dcd692af856621b1d7650c7
Ali Valipour <ali.valipour@email.kntu.ac.ir>
2023-11-13 23:57:41 +0330

    feat: add ToastProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,36 @@ import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Loader } from "./components/Loader";
+import { ToastProvider } from "./components";
 
 const Layout = lazy(() => import("./layout"));
 const New = lazy(() => import("./pages/New"));
 
 const App = () => {
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Layout />
-            </Suspense>
-          }
-        >
+    <ToastProvider>
+      <BrowserRouter basename="/">
+        <Routes>
           <Route
-            path="new"
+            path="/"
             element={
               <Suspense fallback={<Loader />}>
-                <New />
+                <Layout />
               </Suspense>
             }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          >
+            <Route
+              path="new"
+              element={
+                <Suspense fallback={<Loader />}>
+                  <New />
+                </Suspense>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ToastProvider>
   );
 };
 
