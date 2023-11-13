444cb118ed88508b45ba63659bca30e83eff60cc
Ali Valipour <ali.valipour@email.kntu.ac.ir>
2023-11-14 00:49:50 +0330

    fix: type errors

--- src/components/Toast/Toast.context.ts
+++ src/components/Toast/Toast.context.ts
@@ -3,6 +3,7 @@ import { createContext } from "react";
 import { UseToastReturnType } from "./useToast";
 
 export const ToastContext = createContext<UseToastReturnType>({
+  notificationText: "",
   showNotification: () => {},
   showState: [false, () => {}],
 });
